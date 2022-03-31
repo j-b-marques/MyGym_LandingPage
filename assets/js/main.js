6cad9b1597329b9c9647fffc6c0c2f6df25af19b
João Marquês <99401638+j-b-marques@users.noreply.github.com>
2022-03-31 19:45:52 +0100

    Comit

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,68 @@ const submitBtn = document.querySelector('.submit__btn');
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault(); //* for demo purposes
 });
+
+//* SCROLL REVEAL
+ScrollReveal().reveal('nav ', {
+  delay: 600,
+  easing: 'ease-in',
+});
+ScrollReveal().reveal('.header__lead', {
+  delay: 300,
+  distance: '200px',
+  origin: 'left',
+  duration: 600,
+  easing: 'ease-in',
+});
+ScrollReveal().reveal('.header__bottom', {
+  delay: 300,
+  distance: '200px',
+  origin: 'left',
+  duration: 600,
+  easing: 'ease-in',
+});
+//*****/
+ScrollReveal().reveal('.img__wrapper', {
+  delay: 300,
+  duration: 600,
+  easing: 'ease-in',
+});
+ScrollReveal().reveal('.img__wrapper2', {
+  delay: 300,
+
+  duration: 600,
+  easing: 'ease-in',
+});
+
+//********/
+
+ScrollReveal().reveal('.join__us', {
+  delay: 600,
+  easing: 'ease-in',
+});
+ScrollReveal().reveal('video', {
+  rotate: {
+    y: 180,
+  },
+  duration: 2200,
+});
+
+//*****/
+ScrollReveal().reveal('.flex__ct__modalidades', {
+  delay: 600,
+  easing: 'ease-in',
+});
+
+ScrollReveal().reveal('.form__wrapper', {
+  delay: 300,
+  distance: '200px',
+  origin: 'right',
+  duration: 600,
+  easing: 'ease-in',
+});
+
+//*****/
+ScrollReveal().reveal('.footer__ct', {
+  delay: 600,
+  easing: 'ease-in',
+});
